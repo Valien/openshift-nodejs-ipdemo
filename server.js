f20dd864796f5f969b8cf70e3cfe0988661a6763
Allen Vailliencourt <chumpmonkey@gmail.com>
2017-10-23 15:35:13 -0400

    Added hostname info.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require("http"),
-    ip   = require("ip");
+    ip   = require("ip"),
+    os   = require("os");
 
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello OpenShift!\n");
-    response.write("Your IP is: " + ip.address());
+    response.write("Your IP is: " + ip.address() + "\n");
+    response.write("The pod hostname is: " + os.hostname());
     response.end();
 }).listen(8080);

2f433cbd147ddbe6f4c780181a66322202f191f6
Allen Vailliencourt <chumpmonkey@gmail.com>
2017-11-03 15:19:30 -0400

    Added new line.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello OpenShift!\n");
     response.write("The Pod IP is: " + ip.address() + "\n");
-    response.write("The Pod hostname is: " + os.hostname());
+    response.write("The Pod hostname is: " + os.hostname() + "\n");
+    response.write("this is a pod");  
     response.end();
 }).listen(8080);

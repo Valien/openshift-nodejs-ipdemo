1c3704bb168496c8e3249ca9cdd7dc9e12f56868
Allen Vailliencourt <chumpmonkey@gmail.com>
2017-11-03 15:22:06 -0400

    Fixed broken file

--- server.js
+++ server.js
@@ -6,12 +6,7 @@ http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello OpenShift!\n");
     response.write("The Pod IP is: " + ip.address() + "\n");
-<<<<<<< HEAD
     response.write("The Pod hostname is: " + os.hostname() + "\n");
     response.write("this is a pod");  
-=======
-    response.write("The Pod hostname is: " + os.hostname());
-    response.write("this is a demo");
->>>>>>> origin/master
     response.end();
 }).listen(8080);
